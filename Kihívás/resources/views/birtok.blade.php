a12e8cf0b0962c0806d7d180a28576cf6d05db3d
Eroster-web <bicskamicska@gmail.com>
2021-08-06 13:25:12 +0200

    final 2.1.0

--- Kihívás/resources/views/birtok.blade.php
+++ Kihívás/resources/views/birtok.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="icon" href="{{ asset('img/icon.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>A birtok - Juliet Victor Winery</title>
     <!--Ion icons-->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
